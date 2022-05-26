0c57bdb856b679a9c681b6ddf6445ecaa4233282
Amanda Engel <amandaengeldecastro@gmail.com>
2022-05-26 18:08:48 -0300

    HashMap - Receita

--- src/TerceiroDesafio/Receita.java
+++ src/TerceiroDesafio/Receita.java
@@ -1,12 +1,13 @@
 package TerceiroDesafio;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Receita {
 	public static void main (String[]args) {
 
-		String receita;
+		int receita = 1 ;
 		
 		List<String> ingrediente = new ArrayList<>();
 		ingrediente.add("ovos");
@@ -17,8 +18,23 @@ public class Receita {
 		ingrediente.add("fermento");
 		ingrediente.add("leite");
 
-		System.out.println("Bolo" + ingrediente);
 
+		switch (receita) {
+		case 1: 
+			System.out.println("Bolo" + ingrediente);
+		}
 		
+		HashMap<Integer, String> qtdEIngredientes = new HashMap<Integer, String>();
+		qtdEIngredientes.put(3, "ovos");
+		qtdEIngredientes.put(1, "chocolate em po");
+		qtdEIngredientes.put(4, "manteiga");
+		qtdEIngredientes.put(2, "farinha");		
+		qtdEIngredientes.put(2, "açucar");
+		qtdEIngredientes.put(1, "açucar");
+		qtdEIngredientes.put(1, "leite");
+		
+		System.out.println("A quantidade de ingredientes necessárias para a receita são: "+ qtdEIngredientes);
+
+
 	}
 }
